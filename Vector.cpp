d7a86268850a02ae385d8b2e3104a1b6925a711e
KuznetsLex <kuznets.lex@gmail.com>
2024-03-15 00:37:07 +0600

    fully completed and untested (without main) lab1 without extras

--- Vector.cpp
+++ Vector.cpp
@@ -37,12 +37,124 @@ size_t Vector::getLen() const {
     return len;
 }
 
+void Vector::setLen(size_t len_) {
+    assert((len > 0) && "ERROR: length should be positive");
+    Vector::len = len_;
+}
+
 int *Vector::getArr() const {
-    int* copyArr = new int[len];
+    int *copyArr = new int[len];
     for (int i = 0; i < len; i++) {
         copyArr[i] = arr[i];
     }
     return copyArr;
+}
+
+int &Vector::operator[](int i) const {
+    assert((i >= 0) && "ERROR: Wrong input! Index should be nonnegative.");
+    return arr[i];
+}
+
+void Vector::resize(size_t newSize) {
+    int *newArr = new int[newSize];
+    for (int i = 0; i < newSize; i++) {
+        if (i < len) {
+            newArr[i] = arr[i];
+        } else {
+            newArr[i] = 0;
+        }
+    }
+    delete[] arr;
+    arr = newArr;
+    len = newSize;
+}
+
+Vector &Vector::operator=(Vector &&other) {
+    if (this == &other) { return *this; }
+    std::swap(arr, other.arr);
+    std::swap(len, other.len);
+    return *this;
+}
+
+Vector &Vector::operator=(const Vector &vector) {
+    if (this == &vector) { return *this; }
+    Vector copy(vector);
+    std::swap(arr, copy.arr);
+    std::swap(len, copy.len);
+    return *this;
+}
+
+bool Vector::operator==(const Vector &vector) {
+    if (len != vector.len) { throw ComparedDifferentLengthException("ERROR: Compared vectors have different length!"); }
+    for (int i = 0; i < len; i++) {
+        if (arr[i] != vector.arr[i]) { return false; }
+    }
+    return true;
+}
+
+bool Vector::operator!=(const Vector &vector) {
+    if (len != vector.len) { throw ComparedDifferentLengthException("ERROR: Compared vectors have different length!"); }
+    for (int i = 0; i < len; i++) {
+        if (arr[i] == vector.arr[i]) { return false; }
+    }
+    return true;
+}
+
+bool Vector::operator<(const Vector &vector) {
+    int minLen = int(vector.len);
+    if (len < minLen) { minLen = int(len); }
+    for (int i = 0; i < minLen; i++) {
+        if (arr[i] >= vector.arr[i]) { return false; }
+    }
+    return true;
+}
+
+bool Vector::operator<=(const Vector &vector) {
+    int minLen = int(vector.len);
+    if (len < minLen) { minLen = int(len); }
+    for (int i = 0; i < minLen; i++) {
+        if (arr[i] > vector.arr[i]) { return false; }
+    }
+    if (len > vector.len) { return false; }
+    return true;
+}
+
+bool Vector::operator>(const Vector &vector) {
+    int minLen = int(vector.len);
+    if (len < minLen) { minLen = int(len); }
+    for (int i = 0; i < minLen; i++) {
+        if (arr[i] <= vector.arr[i]) { return false; }
+    }
+    return true;
+}
+
+bool Vector::operator>=(const Vector &vector) {
+    size_t minLen = vector.len;
+    if (len < minLen) { minLen = len; }
+    for (int i = 0; i < minLen; i++) {
+        if (arr[i] < vector.arr[i]) { return false; }
+    }
+    if (len < vector.len) { return false; }
+    return true;
+}
+
+Vector Vector::operator+(const Vector &vector) const {
+    size_t maxLen = vector.len;
+    size_t minLen = len;
+    if (len > maxLen) {
+        maxLen = len;
+        minLen = vector.len;
+    }
+    Vector sumVector(maxLen);
+    for (int i = 0; i < maxLen; i++) {
+        if (i < minLen) {
+            sumVector[i] = arr[i] + vector[i];
+        } else {
+            if (maxLen == len) { sumVector[i] = arr[i]; }
+            else { sumVector[i] = vector.arr[i]; }
+        }
+    }
+    return sumVector;
 }
 
 std::ostream &operator<<(std::ostream &out, const Vector &vector) {
@@ -53,4 +165,18 @@ std::ostream &operator<<(std::ostream &out, const Vector &vector) {
     }
     out << std::endl;
     return out;
+}
+
+std::istream &operator>>(std::istream &in, Vector &vector) {
+    size_t len;
+    int elem;
+    std::cout << "len: ";
+    in >> len;
+    vector.setLen(len);
+    vector.resize(len);
+    for (int i = 0; i < len; i++) {
+        in >> elem;
+        vector[i] = elem;
+    }
+    return in;
 }
--- Vector.h
+++ Vector.h
@@ -5,18 +5,58 @@
 
 class Vector {
 private:
-    int* arr;
+    int *arr;
     size_t len;
 public:
     explicit Vector(size_t size = 10);
-    Vector(size_t size, int n);
-    Vector(Vector const &vector);
-    Vector(Vector&& vector);
-    ~Vector();
-    size_t getLen() const;
-    int* getArr() const;
-};
-std::ostream &operator<<(std::ostream &out, const Vector &vector);
 
+    Vector(size_t size, int n);
+
+    Vector(Vector const &vector);
+
+    Vector(Vector &&vector);
+
+    ~Vector();
+
+    size_t getLen() const;
+
+    void setLen(size_t len_);
+
+    int *getArr() const;
+
+    int &operator[](int i) const;
+
+    void resize(size_t newSize);
+
+    Vector &operator=(const Vector &vector);
+
+    Vector &operator=(Vector &&other);
+
+    bool operator==(const Vector &vector);
+
+    bool operator!=(const Vector &vector);
+
+    class ComparedDifferentLengthException : public std::exception {
+        const char *message;
+    public:
+        ComparedDifferentLengthException(const char *theMessage) : message(theMessage) {}
+
+        const char *what() const noexcept { return message; }
+    };
+
+    bool operator<(const Vector &vector);
+
+    bool operator<=(const Vector &vector);
+
+    bool operator>(const Vector &vector);
+
+    bool operator>=(const Vector &vector);
+
+    Vector operator+(const Vector &vector) const;
+};
+
+std::istream &operator>>(std::istream &in, Vector &vector);
+
+std::ostream &operator<<(std::ostream &out, const Vector &vector);
 
 #endif
